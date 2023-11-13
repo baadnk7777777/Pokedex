42a89084fc8c14f48208c7dae8f2551f56ccdf45
Meliodas <banktv76@gmail.com>
2023-11-14 04:30:15 +0700

    fix ui.

--- src/app/Presentation/Pokedex/components/PokedexHeader.tsx
+++ src/app/Presentation/Pokedex/components/PokedexHeader.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function PokedexHeader() {
   return (
     <div className="bg-red-500 flex w-full h-10 md:px-20 px-10">
-    <div className=" text-white items-center flex   ">
-        <p className='text-blue-400'>CloudNC</p>
-        <p className='pl-2'>Pokedex Challenge</p>
+      <div className=" text-white items-center flex font-bold   ">
+        <p className="text-blue-400">CloudNC</p>
+        <p className="pl-2">Pokedex Challenge</p>
+      </div>
     </div>
-</div>
-  )
+  );
 }
 
-export default PokedexHeader
+export default PokedexHeader;
